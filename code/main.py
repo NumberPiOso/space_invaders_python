b984bebc48ee4383e9424ae0c13c8199847b2b2b
Pablo <osoriopabl@gmail.com>
2020-01-30 17:48:53 -0500

    last version today, problem with keys

--- code/main.py
+++ code/main.py
@@ -1,12 +1,10 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
-from kivy.properties import (
-    NumericProperty, ReferenceListProperty, ObjectProperty
-)
+from kivy.properties import NumericProperty, ReferenceListProperty
 from kivy.vector import Vector
-from kivy.clock import Clock
+from kivy.core.window import Window
 
-class AsteroidsPlayer(Widget):
+class AsteroidsBall(Widget):
     velocity_x = NumericProperty(0)
     velocity_y = NumericProperty(0)
     velocity = ReferenceListProperty(velocity_x, velocity_y)
@@ -14,25 +12,31 @@ class AsteroidsPlayer(Widget):
     def move(self):
         self.pos = Vector(*self.velocity) + self.pos
 
+
 class AsteroidsGame(Widget):
     player = ObjectProperty(None)
 
+    
     def init(self):
+        self.player.center
         self.player.center = self.center
 
-    def update(self, dt):
-        return False
+# #     def update(self, dt):
+# #         self.player.move()
 
-    def on_touch_move(self, touch):
-        self.player.center_x = touch.x
-        self.player.center_y = touch.y
+# #     def on_touch_move(self, touch):
+# #         self.player.center_x = touch.x
+# #         self.player.center_y = touch.y
 
-class AsteriodsApp(App):
+import tkinter
+
+class AsteroidsApp(App):
     def build(self):
-        game = AsteroidsGame()
-        game.init()
-        Clock.schedule_interval(game.update, 1.0 / 60.0)
-        return game
+#         game = Label(text='Hello World')
+#         # game = AsteroidsGame()
+#         # game.init()
+#         # Clock.schedule_interval(game.update, 1.0 / 60.0)
+        return AsteroidsGame()
 
 if __name__ == '__main__':
-    AsteriodsApp().run()
+    AsteroidsApp().run()
